c7841cdddce3e72961f43c6c19181bfdad48b893
linuxwt <wt439757183@126.com>
2018-09-01 19:02:06 +0800

    提交文件

---
+++ installECS.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#Date: 2018-08-14
+
+## fdisk and mount
+echo "d  
+n  
+p  
+1
+
+
+w  
+" | fdisk $1
+mkfs.xfs -f ${1}1  
+mkdir /data/  
+mount ${1}1 /data  
+mkdir -p /data/gooalgene
+
+##  enable firewalld.
+function_firewalld () {  
+firewall-cmd --zone=public --add-port=43222/tcp --permanent  
+firewall-cmd --zone=public --add-port=80-90/tcp --permanent  
+firewall-cmd --zone=public --add-port=150/tcp --permanent  
+firewall-cmd --zone=public --add-port=27117-27120/tcp --permanent  
+firewall-cmd --zone=public --add-port=33066-33070/tcp --permanent  
+}
+firewalld_status=$(systemctl status firewalld)  
+if [ ${firewalld_status == "success"} ];then  
+    function_firewalld
+else  
+    systemctl start firewalld
+    function_firewalld
+fi  
+firewall-cmd --reload  
+systemctl restart firewalld  
+systemctl enable firewalld  
+systemctl daemon-reload
+
+##  modify sshd_config
+
+cp -ap /etc/fstab /etc/fstab.bak
+
+echo "/dev/vdb1 /data xfs defaults 0 0"  >> /etc/fstab
+
+cp -ap /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+sed -i '/#Port 22/a\Port 43222' /etc/ssh/sshd_config
+
+sed -i 's!#PermitEmptyPasswords!PermitEmptyPasswords!g' /etc/ssh/sshd_config
+
+systemctl restart sshd  
+systemctl enable sshd  
+systemctl daemon-reload
+
+## 4. install packages and start docker service.
+
+yum -y install epel-release && yum -y install docker && yum install -y python-pip  
+yum -y install rsync lrzsz mlocate git openssh-clients wget
+
+pip install docker-compose
+
+echo "{\"registry-mirrors\": [\"https://nr630v1c.mirror.aliyuncs.com\"]}" > /etc/docker/daemon.json
+
+systemctl start docker
+
+systemctl enable docker
+
+systemctl daemon-reload  
+## 5. create user.
+
+groupadd docker
+
+useradd -G docker tengwang
+
+passwd tengwang
+
+systemctl restart docker  
+
+chown -R tengwang.tengwang /data/gooalgene  
+usermod -g dockerroot tengwang  
+systemctl restart docker  
+# close selinux
+sed -i 's/enforcing/disabled/g' /etc/selinux/config  
+sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux  
+reboot  
---
+++ tomcatdeploy.sh
@@ -0,0 +1,342 @@
+#!/bin/bash
+
+tomcat_dir="/data/gooalgene/java"
+if [ ! -d ${tomcat_dir} ];then
+    mkdir -p ${tomcat_dir}
+else
+    mv ${tomcat_dir} ${tomcat_dir}.bak
+    mkdir -p ${tomcat_dir}
+fi
+
+# 宿主机上部署jdk和maven
+prog=$(rpm -qa|grep java | wc -l)
+if [ ${prog} -gt 0 ];then
+    echo "the system have java,next we uninstall it and install new."
+    sleep 5
+    rpm -qa|grep java | xargs rpm -e --nodeps 
+else
+    echo "you need installed java"
+fi
+cd ${tomcat_dir}
+wget  http://apache.fayea.com/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+tar zvxf apache-maven-3.5.4-bin.tar.gz 
+mv apache-maven-3.5.4 maven3.5
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
+tar zvxf jdk-8u181-linux-x64.tar.gz
+mv jdk1.8.0_181 jdk1.8
+cp /etc/profile /etc/profile.bak
+cat <<EOF>> /etc/profile
+export JAVA_HOME=/data/gooalgene/java/jdk1.8 MAVEN_HOME=/data/gooalgene/java/maven3.5
+export CLASSPATH=.:\$JAVA_HOME/jre/lib/rt.jar:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar
+export PATH=\$JAVA_HOME/bin:\$MAVEN_HOME/bin:\$PATH
+EOF
+
+# 生成带maven的tommcat镜像
+cat <<EOF>> ${tomcat_dir}/Dockerfile
+FROM tomcat:8.5  
+MAINTAINER linuxwt
+
+RUN mkdir /usr/local/tomcat/maven  
+ADD ./maven3.5 /usr/local/tomcat/maven
+
+ENV MAVEN_HOME /usr/local/tomcat/maven  
+ENV PATH \$PATH:\$MAVEN_HOME/bin
+
+EXPOSE 8080  
+EOF
+docker build -t tomcat:maven .
+# 一般我们会在tomcat前面加上一个nginx作反向代理已达到安全的目的,下面是我们编写的docker-compose.yml文件、
+cat <<EOF>> ${tomcat_dir}/docker-compose.yml
+nginx_linuxwt:
+   restart: always
+   image: nginx
+   container_name: nginx_linuxwt
+   volumes:
+       - \$PWD/conf/nginx.conf:/etc/nginx/nginx.conf
+       - \$PWD/conf/nginx_reverse.conf:/etc/nginx/conf.d/default.conf
+       - /etc/localtime:/etc/timezone
+       - /www/html:/www/html
+   links:
+       - tomcat_linuxwt
+   privileged: true
+   ports:
+       - 150:80
+       - 80:90
+tomcat_linuxwt:
+   restart: always
+   image: tomcat:maven
+   container_name: tomcat_linuxwt
+   volumes:
+       - \$PWD/conf/server.xml:/usr/local/tomcat/conf/server.xml
+       - /webapps:/usr/local/tomcat/webapps/root
+       - /etc/localtime:/etc/localtime
+       - /etc/timezone:/etc/timezone
+   expose:
+       - 8080
+EOF
+# 编写nginx.conf
+mkdir -p ${tomcat_dir}/conf
+touch ${tomcat_dir}/conf/nginx.conf
+cat <<EOF>> ${tomcat_dir}/conf/nginx.conf
+user  nginx;  
+worker_processes  1;
+
+error_log  /var/log/nginx/error.log warn;  
+pid        /var/run/nginx.pid;
+
+
+events {  
+    worker_connections  1024;
+}
+
+
+http {  
+    include       /etc/nginx/mime.types;
+        server_tokens off;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  3000;
+
+    gzip  on;
+    gzip_min_length  1k;
+    gzip_buffers     4 16k;
+    gzip_http_version 1.1;
+    gzip_comp_level 2;
+    #  下面是一整段
+    gzip_types text/plain image/png  application/javascript application/x-javascript  text/javascript text/css application/xml image/x-icon application/xml+rss 
+    application/json; 
+    gzip_vary on;
+    gzip_proxied   expired no-cache no-store private auth;
+    gzip_disable   "MSIE [1-6]\.";
+
+    include /etc/nginx/conf.d/*.conf;
+}
+EOF
+
+# 编写nginx_reverse.conf
+touch ${tomcat_dir}/conf/nginx_reverse.conf
+cat <<EOF>> ${tomcat_dir}/conf/nginx_reverse.conf
+upstream tomcat_linuxwt {  
+    server tomcat_linuxwt:8080;
+    }
+
+server {  
+    listen  80;
+    location / {
+        root /www/html;
+        index index.html;
+        try_files \$uri \$uri/ /index.html;
+        autoindex on;
+        autoindex_exact_size off;
+        autoindex_localtime on;
+    }
+}
+
+map \$http_upgrade \$connection_upgrade {  
+        default upgrade;
+        ''      close;
+    }
+
+server {  
+    listen  90;
+    location / {
+        proxy_set_header Access-Control-Allow-Origin *;
+        proxy_pass_header Server;
+        proxy_set_header Host \$http_host;
+        proxy_redirect off;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Scheme \$scheme;
+        proxy_pass http://tomcat_linuxwt;
+        client_max_body_size 0;
+        proxy_connect_timeout 420;
+        proxy_send_timeout 420;
+        proxy_read_timeout 420;
+        autoindex on;
+        autoindex_exact_size off;
+        autoindex_localtime on;
+    }
+}
+EOF
+
+# 编写server.xml
+touch ${tomcat_dir}/conf/server.xml
+cat <<EOF>> ${tomcat_dir}/conf/server.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+  Licensed to the Apache Software Foundation (ASF) under one or more
+  contributor license agreements.  See the NOTICE file distributed with
+  this work for additional information regarding copyright ownership.
+  The ASF licenses this file to You under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with
+  the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+<!-- Note:  A "Server" is not itself a "Container", so you may not
+     define subcomponents such as "Valves" at this level.
+     Documentation at /docs/config/server.html
+ -->
+<Server port="8005" shutdown="SHUTDOWN">
+  <Listener className="org.apache.catalina.startup.VersionLoggerListener" />
+  <!-- Security listener. Documentation at /docs/config/listeners.html
+  <Listener className="org.apache.catalina.security.SecurityListener" />
+  -->
+  <!--APR library loader. Documentation at /docs/apr.html -->
+  <Listener className="org.apache.catalina.core.AprLifecycleListener" SSLEngine="on" />
+  <!-- Prevent memory leaks due to use of particular java/javax APIs-->
+  <Listener className="org.apache.catalina.core.JreMemoryLeakPreventionListener" />
+  <Listener className="org.apache.catalina.mbeans.GlobalResourcesLifecycleListener" />
+  <Listener className="org.apache.catalina.core.ThreadLocalLeakPreventionListener" />
+
+  <!-- Global JNDI resources
+       Documentation at /docs/jndi-resources-howto.html
+  -->
+  <GlobalNamingResources>
+    <!-- Editable user database that can also be used by
+         UserDatabaseRealm to authenticate users
+    -->
+    <Resource name="UserDatabase" auth="Container"
+              type="org.apache.catalina.UserDatabase"
+              description="User database that can be updated and saved"
+              factory="org.apache.catalina.users.MemoryUserDatabaseFactory"
+              pathname="conf/tomcat-users.xml" />
+  </GlobalNamingResources>
+
+  <!-- A "Service" is a collection of one or more "Connectors" that share
+       a single "Container" Note:  A "Service" is not itself a "Container",
+       so you may not define subcomponents such as "Valves" at this level.
+       Documentation at /docs/config/service.html
+   -->
+  <Service name="Catalina">
+
+    <!--The connectors can use a shared executor, you can define one or more named thread pools-->
+    <!--
+    <Executor name="tomcatThreadPool" namePrefix="catalina-exec-"
+        maxThreads="150" minSpareThreads="4"/>
+    -->
+
+
+    <!-- A "Connector" represents an endpoint by which requests are received
+         and responses are returned. Documentation at :
+         Java HTTP Connector: /docs/config/http.html
+         Java AJP  Connector: /docs/config/ajp.html
+         APR (HTTP/AJP) Connector: /docs/apr.html
+         Define a non-SSL/TLS HTTP/1.1 Connector on port 8080
+    -->
+    <Connector port="8080" protocol="HTTP/1.1"
+               connectionTimeout="20000"
+               redirectPort="8443" />
+    <!-- A "Connector" using the shared thread pool-->
+    <!--
+    <Connector executor="tomcatThreadPool"
+               port="8080" protocol="HTTP/1.1"
+               connectionTimeout="20000"
+               redirectPort="8443" />
+    -->
+    <!-- Define a SSL/TLS HTTP/1.1 Connector on port 8443
+         This connector uses the NIO implementation. The default
+         SSLImplementation will depend on the presence of the APR/native
+         library and the useOpenSSL attribute of the
+         AprLifecycleListener.
+         Either JSSE or OpenSSL style configuration may be used regardless of
+         the SSLImplementation selected. JSSE style configuration is used below.
+    -->
+    <!--
+    <Connector port="8443" protocol="org.apache.coyote.http11.Http11NioProtocol"
+               maxThreads="150" SSLEnabled="true">
+        <SSLHostConfig>
+            <Certificate certificateKeystoreFile="conf/localhost-rsa.jks"
+                         type="RSA" />
+        </SSLHostConfig>
+    </Connector>
+    -->
+    <!-- Define a SSL/TLS HTTP/1.1 Connector on port 8443 with HTTP/2
+         This connector uses the APR/native implementation which always uses
+         OpenSSL for TLS.
+         Either JSSE or OpenSSL style configuration may be used. OpenSSL style
+         configuration is used below.
+    -->
+    <!--
+    <Connector port="8443" protocol="org.apache.coyote.http11.Http11AprProtocol"
+               maxThreads="150" SSLEnabled="true" >
+        <UpgradeProtocol className="org.apache.coyote.http2.Http2Protocol" />
+        <SSLHostConfig>
+            <Certificate certificateKeyFile="conf/localhost-rsa-key.pem"
+                         certificateFile="conf/localhost-rsa-cert.pem"
+                         certificateChainFile="conf/localhost-rsa-chain.pem"
+                         type="RSA" />
+        </SSLHostConfig>
+    </Connector>
+    -->
+
+    <!-- Define an AJP 1.3 Connector on port 8009 -->
+    <Connector port="8009" protocol="AJP/1.3" redirectPort="8443" />
+
+
+    <!-- An Engine represents the entry point (within Catalina) that processes
+         every request.  The Engine implementation for Tomcat stand alone
+         analyzes the HTTP headers included with the request, and passes them
+         on to the appropriate Host (virtual host).
+         Documentation at /docs/config/engine.html -->
+
+    <!-- You should set jvmRoute to support load-balancing via AJP ie :
+    <Engine name="Catalina" defaultHost="localhost" jvmRoute="jvm1">
+    -->
+    <Engine name="Catalina" defaultHost="localhost">
+
+      <!--For clustering, please take a look at documentation at:
+          /docs/cluster-howto.html  (simple how to)
+          /docs/config/cluster.html (reference documentation) -->
+      <!--
+      <Cluster className="org.apache.catalina.ha.tcp.SimpleTcpCluster"/>
+      -->
+
+      <!-- Use the LockOutRealm to prevent attempts to guess user passwords
+           via a brute-force attack -->
+      <Realm className="org.apache.catalina.realm.LockOutRealm">
+        <!-- This Realm uses the UserDatabase configured in the global JNDI
+             resources under the key "UserDatabase".  Any edits
+             that are performed against this UserDatabase are immediately
+             available for use by the Realm.  -->
+        <Realm className="org.apache.catalina.realm.UserDatabaseRealm"
+               resourceName="UserDatabase"/>
+      </Realm>
+
+      <Host name="localhost"  appBase="webapps"
+            unpackWARs="true" autoDeploy="true">
+
+        <!-- SingleSignOn valve, share authentication between web applications
+             Documentation at: /docs/config/valve.html -->
+        <!--
+        <Valve className="org.apache.catalina.authenticator.SingleSignOn" />
+        -->
+
+        <!-- Access log processes all example.
+             Documentation at: /docs/config/valve.html
+             Note: The pattern used is equivalent to using pattern="common" -->
+        <Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs"
+               prefix="localhost_access_log" suffix=".txt"
+               pattern="%h %l %u %t &quot;%r&quot; %s %b" />
+
+      </Host>
+    </Engine>
+  </Service>
+</Server>
+EOF
+docker-compose up -d
+
