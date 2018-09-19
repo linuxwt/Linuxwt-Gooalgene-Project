51fe2572e8e11467b41c476943597c0436d718da
linuxwt <31132875+linuxwt@users.noreply.github.com>
2018-09-19 19:37:11 +0800

    Update installECS.sh

--- installECS.sh
+++ installECS.sh
@@ -3,6 +3,10 @@
 #Date: 2018-08-14
 
 ## fdisk and mount
+if [ $# -lt 1 ];then
+        echo "sorry,ou need the argument,the argument should be the name of a new disk."
+        exit -1
+fi
 echo "d  
 n  
 p  
@@ -24,8 +28,9 @@ firewall-cmd --zone=public --add-port=150/tcp --permanent
 firewall-cmd --zone=public --add-port=27117-27120/tcp --permanent  
 firewall-cmd --zone=public --add-port=33066-33070/tcp --permanent  
 }
-firewalld_status=$(systemctl status firewalld)  
-if [ ${firewalld_status} == "success" ];then  
+firewalld_status=$(systemctl status firewalld | grep 'Active' | awk '{print $3}' | awk -F '(' '{print $2}' | awk -F ')' '{print $1}')  
+if [ ${firewalld_status} == "running" ];then  
+    echo "the firewalld is running and we will add the necessary ports."
     function_firewalld
 else  
     systemctl start firewalld
@@ -35,12 +40,13 @@ firewall-cmd --reload
 systemctl restart firewalld  
 systemctl enable firewalld  
 systemctl daemon-reload
+firewall-cmd --list-ports
 
 ##  modify sshd_config
 
 cp -ap /etc/fstab /etc/fstab.bak
 
-echo "/dev/vdb1 /data xfs defaults 0 0"  >> /etc/fstab
+echo "/dev/$1 /data xfs defaults 0 0"  >> /etc/fstab
 
 cp -ap /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
@@ -57,6 +63,8 @@ systemctl daemon-reload
 yum -y install epel-release && yum -y install docker && yum install -y python-pip  
 yum -y install rsync lrzsz mlocate git openssh-clients wget
 
+systemctl start docker
+
 pip install docker-compose
 
 echo "{\"registry-mirrors\": [\"https://nr630v1c.mirror.aliyuncs.com\"]}" > /etc/docker/daemon.json
@@ -69,17 +77,13 @@ systemctl daemon-reload
 ## 5. create user.
 
 groupadd docker
-
-useradd -G docker tengwang
-
-passwd tengwang
-
+useradd -G docker gooal 
+echo "yourpassword" | passwd --stdin gooal  
 systemctl restart docker  
-
-chown -R tengwang.tengwang /data/gooalgene  
-usermod -g dockerroot tengwang  
+chown -R gooal.gooal /data/gooalgene  
 systemctl restart docker  
 # close selinux
+setenforce 0
 sed -i 's/enforcing/disabled/g' /etc/selinux/config  
 sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux  
-reboot  
+
